590c68620cd2b13e0603be586d760a3d21aa0253
Ethan <ethan19970601@163.com>
2024-08-26 10:53:43 +0800

    waitpid 的学习进行中

--- lesson19.1/myprocess.c
+++ lesson19.1/myprocess.c
@@ -1,60 +1,41 @@
 #include <stdio.h>
-#include <unistd.h> // _exit()要此头文件，使用方法与 exit()类似
-#include <stdlib.h> // exit(),要此头文件
+#include <unistd.h> 
+#include <stdlib.h> 
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
 void Worker()
 {
-  int cnt = 5;
-  while(cnt)
-  {
-    printf("I am child process, pid: %d, ppid: %d, cnt= %d\n", getpid(),getppid(), cnt--);
-  }
+    int cnt = 5;
+    while (cnt)
+    {
+        printf("I am child process, pid: %d, ppid: %d, cnt= %d\n", getpid(), getppid(), cnt--);
+    }
 }
 
 int main()
 {
-  pid_t id = fork();
-  if (id == 0)
-  {
-    // 子进程
-    Worker();
-    exit(0);
-  } else {
-    // 父进程
-    //sleep(10);
-    printf("wait before\n");
-    pid_t rid = wait(NULL);
-    printf("wait after\n");
-    if (rid == id)
+    pid_t id = fork();
+    if (id == 0)
     {
-      printf("wait success, pid: %d\n", getpid());
+        // 子进程
+        Worker();
+        exit(10);     /***************       我们将退出结果修改为 10     **************/
     }
-  }
-  sleep(10);
-  return 0;
+    else
+    {
+        // 父进程
+        //sleep(10);                
+        printf("wait before\n");
+        int status = 0;     /*************  我们加入这一行，默认设为 0 **************/
+        pid_t rid = waitpid( id, &status,0);  // 这里也可以体会一下 status 输出型参数的用法，
+										      // 把子进程退出时的退出信息通过 status 给我们返回。
+        printf("wait after\n");              
+        if (rid == id)
+        { 
+            printf("wait success, pid: %d, status: %d\n", getpid(), status);
+        }
+    }
+    sleep(10);
+    return 0;
 }
-//int fun()
-//{
-//  printf("call fun function done!\n");
-//  return 11;
-//  // 任意地点调用exit，表示进程退出，不进行后续执行。
-//  // exit(21);
-//}
-//
-//int main()
-//{
-//  printf("you can see me !");
-//  sleep(3); 
-//  _exit(1);
-////  fun();
-//  printf("i am a process, pid: %d, ppid: %d\n", getpid(), getppid());
-//  // 1. exit(参数)， 参数是进程的退出码，类似与 main 函数的return n
-  // _exit(12);
- 
-  // 1. 在 main 函数中直接进行 return
-  // 2. 在其他函数中调用return 表示的是函数调用结束
-  // return 21;
-
